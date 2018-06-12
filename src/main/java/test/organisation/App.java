07262083c947bb58e27f28cc7805c1db74367189
Matt Dooner <matt.dooner@faber.work>
2018-06-12 11:22:52 +0100

    Added quickstart and circleci config

---
+++ src/main/java/test/organisation/App.java
@@ -0,0 +1,13 @@
+package test.organisation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
